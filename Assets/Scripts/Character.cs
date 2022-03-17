8c472e6b6660bbf43e9ea15d73372f2830286982
civilityreigns <100637768+civilityreigns@users.noreply.github.com>
2022-03-16 19:04:54 -0500

    created yarn spinner demo scene
    A scene for testing dialogues created using the Yarn Spinner addon was created. Right now it supports showing/hiding the game's existing characters and positioning them around the screen. The dialogue window uses the default system included with Yarn Spinner.

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class Character : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [YarnCommand("show")]
+    public void ShowCharacter()
+    {
+        gameObject.GetComponent<SpriteRenderer>().enabled = true;
+    }
+
+    [YarnCommand("hide")]
+    public void HideCharacter()
+    {
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+    }
+
+    [YarnCommand("move")]
+    public void MoveCharacter(GameObject stagePosition)
+    {
+        gameObject.transform.position = stagePosition.transform.position;
+    }
+}
