fa58f1430163b6994169dc07457adc95be3bf084
civilityreigns <100637768+civilityreigns@users.noreply.github.com>
2022-03-19 02:08:25 -0500

    added debug window to dialogue demo
    Created a basic "debug window" which allows a user to specify a folder on their computer and load Yarn Spinner scripts into the game.

---
+++ Assets/Scripts/DialogueDebugWindow.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using Yarn.Unity;
+
+public class DialogueDebugWindow : MonoBehaviour
+{
+    // The actual visible portion of the debug window.
+    public GameObject displayPanel;
+
+    // The text input field which holds the address for the desired yarn script folder.
+    public GameObject workspaceInput;
+
+    // The checkbox 
+    public GameObject loadSubfoldersToggle;
+
+    // The Yarn dialogue runner that we're going to load scripts into.
+    public GameObject dialogueRunner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        displayPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            ToggleDebugWindow();
+        }
+    }
+
+    // When the 'load all scripts' button is clicked, all scripts in the designated folder are loaded, compiled into a program,
+    // and added to the Dialogue Runner.
+    public void OnLoadAllScriptsClicked()
+    {
+        Debug.Log("Load All Scripts button clicked.");
+        Debug.Log(workspaceInput.GetComponent<TMP_InputField>().text);
+        string path = workspaceInput.GetComponent<TMP_InputField>().text;
+        //string workspace = workspaceInput.GetComponent<TMP_InputField>().text;
+        
+        // Check if the given path isn't a valid directory.
+        if(!Directory.Exists(path))
+        {
+            // If not, check if it is a valid yarn script file.
+            if(File.Exists(path) && Path.GetExtension(path) == ".yarn")
+            {
+                // If so, get the file's directory and load from there as normal. (TODO: Allow loading of single files)
+                path = Path.GetDirectoryName(path);
+                Debug.LogWarning("Single yarn file detected and changed to a directory.");
+            }
+            else
+            {
+                // If the path isn't a valid directory or file, then log an error and don't bother trying to load any scripts.
+                Debug.LogError("Path given is not a valid directory or yarn script file.");
+                return;
+            }
+        }
+
+        // If the load subfolders checkbox is checked then we search all subdirectories for yarn scripts.
+        IEnumerable<string> yarnScripts;
+        if(loadSubfoldersToggle.GetComponent<Toggle>().isOn)
+        {
+            yarnScripts = Directory.EnumerateFiles(path, "*.yarn", SearchOption.AllDirectories);
+        }
+        // If not, just search the top directory only.
+        else
+        {
+            yarnScripts = Directory.EnumerateFiles(path, "*.yarn", SearchOption.TopDirectoryOnly);
+        }
+        
+        // Pass the list of yarn scripts to the Dialogue Runner's dynamic loader so it can handle compilation.
+        //LoadYarnScripts(yarnScripts);
+        dialogueRunner.GetComponent<DialogueRunner>().Stop();
+        dialogueRunner.GetComponent<DynamicYarnLoader>().LoadScripts(yarnScripts);
+        dialogueRunner.GetComponent<DialogueRunner>().StartDialogue("Start");
+    }
+
+    public void OnReloadAllScriptsClicked()
+    {
+
+    }
+
+    public void ToggleDebugWindow()
+    {
+        displayPanel.SetActive(!displayPanel.activeInHierarchy);
+    }
+
+    private void LoadYarnScripts(IEnumerable<string> scripts)
+    {
+        foreach(string scriptPath in scripts)
+        {
+            Debug.Log(scriptPath);
+        }
+    }
+}
--- Assets/Scripts/DynamicYarnLoader.cs
+++ Assets/Scripts/DynamicYarnLoader.cs
@@ -18,8 +18,99 @@ public class DynamicYarnLoader : MonoBehaviour
 
     public void LoadScript()
     {
-        string assetPath = "C:/Work/HelloYarn.yarn";
+        string assetPath = "C:/Users/Dan/Desktop/Magicon UI Assets/Yarn Scripts/HelloYarn.yarn";
         var sourceText = File.ReadAllText(assetPath);
+        string fileName = System.IO.Path.GetFileNameWithoutExtension(assetPath);
+        
+        Yarn.Program compiledProgram = null;
+        IDictionary<string, Yarn.Compiler.StringInfo> stringTable = null;
+        
+        // Compile the source code into a compiled Yarn program (or
+        // generate a parse error)
+        var compilationJob = CompilationJob.CreateFromString(fileName, sourceText, null);
+        compilationJob.CompilationType = CompilationJob.Type.FullCompilation;
+
+        var result = Yarn.Compiler.Compiler.Compile(compilationJob);
+
+        IEnumerable<Diagnostic> errors = result.Diagnostics.Where(d => d.Severity == Diagnostic.DiagnosticSeverity.Error);
+
+        if (errors.Count() > 0)
+        {
+            Debug.LogError("Bad");
+            /*parseErrorMessages.AddRange(errors.Select(e => {
+                string message = $"{ctx.assetPath}: {e}";
+                ctx.LogImportError($"Error importing {message}");
+                return message;
+            }));*/
+        }
+        else
+        {
+            stringTable = result.StringTable;
+            compiledProgram = result.Program;
+
+            YarnProject yp = YarnProject.CreateInstance<YarnProject>();
+            byte[] compiledBytes = null;
+
+            using (var memoryStream = new MemoryStream())
+            using (var outputStream = new Google.Protobuf.CodedOutputStream(memoryStream))
+            {
+                // Serialize the compiled program to memory
+                compiledProgram.WriteTo(outputStream);
+                outputStream.Flush();
+
+                compiledBytes = memoryStream.ToArray();
+            }
+
+            yp.compiledYarnProgram = compiledBytes;
+            yp.name = "NAME";
+            // Dont know what these do. They were in the proper projects
+            /*yp.searchAssembliesForActions.Add("YarnSpinner.Unity");
+            yp.searchAssembliesForActions.Add("YarnSpinner.Unity.Editor");
+            yp.searchAssembliesForActions.Add("YarnSpinner.Editor.Tests");
+            yp.searchAssembliesForActions.Add("CommandsInAnAssemblyDefinition");
+            yp.searchAssembliesForActions.Add("YarnSpinnerTests");
+            yp.searchAssembliesForActions.Add("PsdPlugin");
+            yp.searchAssembliesForActions.Add("DocCodeExamples");*/
+
+            var newLocalization = ScriptableObject.CreateInstance<Localization>();
+            string defaultLanguage = System.Globalization.CultureInfo.CurrentCulture.Name;
+            newLocalization.LocaleCode = defaultLanguage;
+
+            var stringTableEntries = stringTable.Select(x => new StringTableEntry
+            {
+                ID = x.Key,
+                Language = defaultLanguage,
+                Text = x.Value.text,
+                File = x.Value.fileName,
+                Node = x.Value.nodeName,
+                LineNumber = x.Value.lineNumber.ToString(),
+                Lock = GetHashString(x.Value.text, 8),
+            });
+            // Add these new lines to the localisation's asset
+            foreach (var entry in stringTableEntries)
+            {
+                newLocalization.AddLocalizedString(entry.ID, entry.Text);
+            }
+            
+            yp.baseLocalization = newLocalization;
+            yp.localizations.Add(newLocalization);
+            newLocalization.name = defaultLanguage;
+            dr.SetProject(yp);
+        }
+    }
+
+    public void LoadScripts(IEnumerable<string> fileNames)
+    {
+        // Load the contents of each script file and combine them into a single Yarn script.
+        string contents = "";
+        foreach(string file in fileNames)
+        {
+            contents += File.ReadAllText(file);
+            contents += "\n";
+        }
+
+        string assetPath = "HelloYarn.yarn";
+        var sourceText = contents;
         string fileName = System.IO.Path.GetFileNameWithoutExtension(assetPath);
         
         Yarn.Program compiledProgram = null;
