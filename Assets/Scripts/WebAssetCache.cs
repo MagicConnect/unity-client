536e50a67470bf856292b0f61f33ec8c5449b190
civilityreigns <100637768+civilityreigns@users.noreply.github.com>
2022-03-30 18:29:23 -0500

    WIP: web asset cache

--- Assets/Scripts/WebAssetCache.cs
+++ Assets/Scripts/WebAssetCache.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.IO;
 
 using BestHTTP;
 using Newtonsoft.Json;
@@ -14,12 +15,14 @@ public class WebAssetCache : MonoBehaviour
 
     #region JSON parsed data classes
     // This is the object containing meta information we parse from the JSON manifest.
+    [System.Serializable]
     public class MetaInformation
     {
         public string Hash {get; set;}
     }
 
     // Object containing asset information parsed from the JSON manifest.
+    [System.Serializable]
     public class Asset
     {
         public string Name {get; set;}
@@ -28,6 +31,7 @@ public class WebAssetCache : MonoBehaviour
     }
 
     // This is the object representing the parsed JSON manifest.
+    [System.Serializable]
     public class AssetManifest
     {
         public MetaInformation Meta {get; set;}
@@ -35,6 +39,7 @@ public class WebAssetCache : MonoBehaviour
     }
 
     // This is the object representing the parsed JSON version file.
+    [System.Serializable]
     public class VersionNumber
     {
         public string Version {get; set;}
@@ -65,6 +70,7 @@ public class WebAssetCache : MonoBehaviour
         if(Instance != null)
         {
             Destroy(gameObject);
+            return;
         }
 
         Instance = this;
@@ -74,10 +80,39 @@ public class WebAssetCache : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // First we should load our version and manifest files from disk, if they exist, so that we can compare them
-        // to the results we get back from the server.
+        // First we're going to do a brief cache integrity check. If our version and manifest files exist, load them in.
+        // If not, set the current version to null (if it isn't already). A null version will tell our loader that there is
+        // no cache and we should start from scratch.
+        string versionFilePath = Path.Combine(Application.persistentDataPath, "manifest_version.dat");
+        if(File.Exists(versionFilePath))
+        {
+            LoadVersion();
+            Debug.Log("Cached version data was found.");
+        }
+        else
+        {
+            // If no cached version was found then we can't check to make sure the cache is up to date (technically we can since the version data
+            // exists in the manifest itself, but that's not the intended behavior sooo).
+            Debug.Log("No cached version data was found.");
+        }
 
-        // First we need to check the manifest version against the one we have on file.
+        string manifestFilePath = Path.Combine(Application.persistentDataPath, "manifest.dat");
+        if(File.Exists(manifestFilePath))
+        {
+            LoadManifest();
+            Debug.Log("Cached manifest data was found.");
+        }
+        else
+        {
+            // If there is no cached manifest for some reason (like maybe it was deleted) there is no point trying to load any files,
+            // because we don't know any information about them. Set the version to null to let the loader know what to do.
+            Debug.Log("No cached manifest data was found.");
+            currentVersion = null;
+        }
+
+        // Perform a check to make sure the cached assets exist and/or load them in and update the cache later as necessary.
+
+        // Next we need to check the manifest version against the one we have on file.
         versionRequest = new HTTPRequest(new Uri("https://art.magic-connect.com/version.json"), OnVersionRequestFinished);
         versionRequest.Send();
 
@@ -101,6 +136,19 @@ public class WebAssetCache : MonoBehaviour
         
     }
 
+    // Method handling updating the cache with new data from the server.
+    private void UpdateCache()
+    {
+        CacheVersion();
+        CacheManifest();
+    }
+
+    // Delete everything from the cache and leave an empty folder.
+    private void ClearCache()
+    {
+
+    }
+
     // Callback for our http version request.
     private void OnVersionRequestFinished(HTTPRequest req, HTTPResponse resp)
     {
@@ -109,16 +157,7 @@ public class WebAssetCache : MonoBehaviour
         serverVersion = JsonConvert.DeserializeObject<VersionNumber>(resp.DataAsText);
         Debug.Log("Server manifest version downloaded. Version: " + serverVersion.Version);
 
-        // Load the version file from disk, if it exists.
-
-        // Check the server version against the manifest version we have on file. If there is no manifest version, or there is a version
-        // mismatch, go ahead and download the manifest from the server and proceed from there.
-        // TODO: Check if cached manifest is on disk and perform a check.
-        if((currentVersion != null) && (serverVersion.Version == currentVersion.Version))
-        {
-            // We're good.
-        }
-        else
+        if(currentVersion == null || serverVersion.Version != currentVersion.Version)
         {
             // There's no version on file, or the versions are different.
             Debug.Log("Server manifest version doesn't match version on file. Downloading new manifest.");
@@ -133,10 +172,52 @@ public class WebAssetCache : MonoBehaviour
         serverManifest = JsonConvert.DeserializeObject<AssetManifest>(resp.DataAsText);
         Debug.Log("Server manifest downloaded.");
 
-        // We shouldn't have to load a manifest from disk, because this callback only happens if there was a version issue.
-        // But if something goes wrong, this would be one of the places to check.
+        UpdateCache();
+    }
 
-        
+    private void CacheVersion()
+    {
+        string serializedData = JsonUtility.ToJson(serverVersion);
+        SaveDataToFile("manifest_version.dat", serializedData);
+    }
+
+    private void LoadVersion()
+    {
+        currentVersion = JsonUtility.FromJson<VersionNumber>(LoadDataFromFile("manifest_version.dat"));
+    }
+
+    private void CacheManifest()
+    {
+        string serializedData = JsonUtility.ToJson(serverManifest);
+        SaveDataToFile("manifest.dat", serializedData);
+    }
+
+    private void LoadManifest()
+    {
+        currentManifest = JsonUtility.FromJson<AssetManifest>(LoadDataFromFile("manifest.dat"));
+    }
+
+    // Method which handles writing serialized classes to the persistent data folder.
+    private void SaveDataToFile(string path, string json)
+    {
+        var filePath = Path.Combine(Application.persistentDataPath, path);
+        File.WriteAllText(filePath, json);
+    }
+
+    // Method which handles loading serialized classes from the persistent data folder.
+    private string LoadDataFromFile(string path)
+    {
+        var filePath = Path.Combine(Application.persistentDataPath, path);
+
+        if(File.Exists(filePath))
+        {
+            return File.ReadAllText(filePath);
+        }
+        else
+        {
+            Debug.LogErrorFormat("There is no file under the path {0}", filePath);
+            return "";
+        }
     }
 
     // This method tells the caller if there is a cached asset manifest on file.
