14d3890c52849a3c89b4f9dcb544af56b4084569
civilityreigns <100637768+civilityreigns@users.noreply.github.com>
2022-04-26 08:55:06 -0500

    rearranged debug window

--- Assets/Scripts/DialogueDebugWindow.cs
+++ Assets/Scripts/DialogueDebugWindow.cs
@@ -55,7 +55,7 @@ public class DialogueDebugWindow : MonoBehaviour
         settings = new DebugSettings();
 
         // Load previously saved settings, if they exist.
-        LoadSettings();
+        //LoadSettings();
 
         // Set up the SimpleFileBrowser.
         FileBrowser.SetFilters(false, new FileBrowser.Filter("Yarn Scripts", ".yarn"));
@@ -82,66 +82,6 @@ public class DialogueDebugWindow : MonoBehaviour
     {
         // Get the files and folders to load from the file browser.
         fileBrowserCoroutine = StartCoroutine(ShowLoadDialogueCoroutine());
-
-        /*
-        Debug.Log("Load All Scripts button clicked.");
-        Debug.Log(workspaceInput.GetComponent<TMP_InputField>().text);
-        string path = workspaceInput.GetComponent<TMP_InputField>().text;
-        
-        // Check if the given path isn't a valid directory.
-        if(!Directory.Exists(path))
-        {
-            // If not, check if it is a valid yarn script file.
-            if(File.Exists(path) && Path.GetExtension(path) == ".yarn")
-            {
-                // If so, load the file contents and sent it to the dynamic yarn loader.
-                //path = Path.GetDirectoryName(path);
-                //Debug.LogWarning("Single yarn file detected and changed to a directory.");
-                dialogueRunner.GetComponent<DialogueRunner>().Stop();
-                HideOptionsList();
-                dialogueRunner.GetComponent<DynamicYarnLoader>().LoadScript(File.ReadAllText(path));
-                dialogueRunner.GetComponent<DialogueRunner>().StartDialogue("Start");
-            }
-            else
-            {
-                // If the path isn't a valid directory or file, then log an error and don't bother trying to load any scripts.
-                Debug.LogError("Path given is not a valid directory or yarn script file.");
-                return;
-            }
-        }
-        else
-        {
-            // If the load subfolders checkbox is checked then we search all subdirectories for yarn scripts.
-            IEnumerable<string> yarnScripts;
-            if(loadSubfoldersToggle.GetComponent<Toggle>().isOn)
-            {
-                yarnScripts = Directory.EnumerateFiles(path, "*.yarn", SearchOption.AllDirectories);
-            }
-            // If not, just search the top directory only.
-            else
-            {
-                yarnScripts = Directory.EnumerateFiles(path, "*.yarn", SearchOption.TopDirectoryOnly);
-            }
-
-            // Load the contents of each script file and combine them into a single Yarn script.
-            string contents = "";
-            foreach(string file in yarnScripts)
-            {
-                contents += File.ReadAllText(file);
-                contents += "\n";
-            }
-            
-            // Pass the contents of yarn script(s) to the Dialogue Runner's dynamic loader so it can handle compilation.
-            dialogueRunner.GetComponent<DialogueRunner>().Stop();
-            HideOptionsList();
-            dialogueRunner.GetComponent<DynamicYarnLoader>().LoadScript(contents);
-            dialogueRunner.GetComponent<DialogueRunner>().StartDialogue("Start");
-        }
-        
-
-        RefreshNodeDropdown();
-        SaveSettings();
-        */
     }
 
     public void OnStartAtNodeClicked()
@@ -293,7 +233,7 @@ public class DialogueDebugWindow : MonoBehaviour
 
             // TODO: Repurpose the debug settings code or disable it entirely for now. It looks like the simple file browser automatically keeps track of
             // the last directory it was used in, and having a 'load subfolder' toggle doesn't seem useful now that the filebrowser is much simpler to use.
-            SaveSettings();
+            //SaveSettings();
         }
 
         // This coroutine is done, so nullify the reference so everyone else knows.
