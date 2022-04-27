6237472425010726c5fe886400e0e66a5d35b9c7
civilityreigns <100637768+civilityreigns@users.noreply.github.com>
2022-04-27 18:48:55 -0500

    #10 created new fade_in and fade_out commands
    #10 New 'fade_in_character' and 'fade_out_character' commands added. Characters can now be shown and hidden via an animation, with optional color value parameters to configure what color the characters fade into or out of.

--- Assets/Scripts/ConversationCharacter.cs
+++ Assets/Scripts/ConversationCharacter.cs
@@ -21,6 +21,8 @@ public class ConversationCharacter : MonoBehaviour
     Coroutine dimmingCoroutine;
     Coroutine undimmingCoroutine;
     Coroutine movingCoroutine;
+    Coroutine fadeInCoroutine;
+    Coroutine fadeOutCoroutine;
 
     void Awake()
     {
@@ -74,18 +76,133 @@ public class ConversationCharacter : MonoBehaviour
         rectTransform.position = stagePosition.GetComponent<RectTransform>().position;
     }
 
+    // Command to fade a character out until they're invisible. Optional rgb values determine what color the character should fade into,
+    // so for example the character can fade to black instead of just turning invisible.
     [YarnCommand("fade_out_character")]
-    public void FadeOut()
+    public IEnumerator FadeOutAsync(float animationTime = 0.2f, bool waitForAnimation = false, float r = 255.0f, float g = 255.0f, float b = 255.0f)
     {
-        //gameObject.GetComponent<SpriteRenderer>().color = Color.gray;
-        characterImage.color = Color.gray;
+        // If there's already a fade out animation running, cancel it.
+        if(fadeOutCoroutine != null)
+        {
+            StopCoroutine(fadeOutCoroutine);
+        }
+
+        // If there's a fade in animation running, cancel it so they don't conflict with one another.
+        if(fadeInCoroutine != null)
+        {
+            // Note: Any cleanup the fade in coroutine would have had to do should be handled here, or
+            // refactored into a special StopFadeInCoroutine() method.
+            StopCoroutine(fadeInCoroutine);
+            fadeInCoroutine = null;
+        }
+
+        fadeOutCoroutine = StartCoroutine(FadeOut(animationTime, r, g, b));
+        
+        if(waitForAnimation)
+        {
+            yield return new WaitUntil(() => fadeOutCoroutine == null);
+        }
     }
 
-    [YarnCommand("fade_in_character")]
-    public void FadeIn()
+    public IEnumerator FadeOut(float animationTime = 0.2f, float r = 255.0f, float g = 255.0f, float b = 255.0f)
     {
-        //gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+        Debug.LogFormat("{0} fading out over {1} seconds.", gameObject.name, animationTime);
+        float timePassed = 0.0f;
+        Color newColor = new Color(r, g, b, 0.0f);
+        Color oldColor = characterImage.color;
+
+        while(timePassed <= animationTime)
+        {
+            float progress;
+
+            // Make sure not to divide by 0.
+            if(animationTime <= 0.0f)
+            {
+                progress = 1.0f;
+            }
+            else
+            {
+                progress = timePassed / animationTime;
+            }
+
+            characterImage.color = Color.Lerp(oldColor, newColor, progress);
+
+            timePassed += Time.deltaTime;
+            yield return null;
+        }
+
+        // Make sure the desired color result is set after the animation completes, in case the timing was off.
+        characterImage.color = newColor;
+
+        // The character is already invisible, but just in case lets deactivate the image too. There might be a performance
+        // cost from having too many transparent objects floating around.
+        HideCharacter();
+
+        fadeOutCoroutine = null;
+        Debug.LogFormat("{0} fade out animation complete.", gameObject.name);
+    }
+
+    // Command to fade a character in until they're visible. Optional rgb value arguments determine what the starting color of the
+    // character should be, so for example the character can fade in from black.
+    [YarnCommand("fade_in_character")]
+    public IEnumerator FadeInAsync(float animationTime = 0.2f, bool waitForAnimation = false, float r = 255.0f, float g = 255.0f, float b = 255.0f)
+    {
+        if(fadeInCoroutine != null)
+        {
+            StopCoroutine(fadeInCoroutine);
+        }
+
+        // If there's a fade out animation running, cancel it so they don't conflict with one another.
+        if(fadeOutCoroutine != null)
+        {
+            // Note: Any cleanup the fade out coroutine would have had to do should be handled here, or
+            // refactored into a special StopFadeInCoroutine() method.
+            StopCoroutine(fadeOutCoroutine);
+            fadeOutCoroutine = null;
+        }
+
+        fadeInCoroutine = StartCoroutine(FadeIn(animationTime, r, g, b));
+        
+        if(waitForAnimation)
+        {
+            yield return new WaitUntil(() => fadeInCoroutine == null);
+        }
+    }
+
+    public IEnumerator FadeIn(float animationTime = 0.2f, float r = 255.0f, float g = 255.0f, float b = 255.0f)
+    {
+        Debug.LogFormat("{0} fading in over {1} seconds.", gameObject.name, animationTime);
+        float timePassed = 0.0f;
+        Color oldColor = new Color(r, g, b, 0.0f);
+
+        // Make sure the character is capable of being seen. By default character images are deactivated until used.
+        ShowCharacter();
+
+        while(timePassed <= animationTime)
+        {
+            float progress;
+
+            // Make sure not to divide by 0.
+            if(animationTime <= 0.0f)
+            {
+                progress = 1.0f;
+            }
+            else
+            {
+                progress = timePassed / animationTime;
+            }
+
+            characterImage.color = Color.Lerp(oldColor, Color.white, progress);
+
+            timePassed += Time.deltaTime;
+            yield return null;
+        }
+
+        // Make sure the desired color result is set after the animation completes, in case the timing was off.
         characterImage.color = Color.white;
+
+        fadeInCoroutine = null;
+        Debug.LogFormat("{0} fade in animation complete.", gameObject.name);
     }
 
     public IEnumerator ShowCharacterAnimation(float timeToComplete)
@@ -112,6 +229,8 @@ public class ConversationCharacter : MonoBehaviour
             StopCoroutine(dimmingCoroutine);
         }
 
+        // TODO: Check for running undim coroutine and shut it down so there's no conflict.
+
         dimmingCoroutine = StartCoroutine(DimCharacter(timeToComplete));
 
         // TODO: The else block may be unnecessary, or at least the yield return is. Right now it's basically waiting
@@ -122,6 +241,8 @@ public class ConversationCharacter : MonoBehaviour
         }
     }
 
+    // TODO: Rename timeToComplete to animationTime for better self-documentation. Do this for every other method that
+    // uses this variable name.
     public IEnumerator DimCharacter(float timeToComplete = 0.2f)
     {
         Debug.LogFormat("{0}: Dimming over {1} seconds.", gameObject.name, timeToComplete);
@@ -130,6 +251,7 @@ public class ConversationCharacter : MonoBehaviour
 
         while(timePassed <= timeToComplete)
         {
+            // TODO: Account for divide by 0 case.
             float progress = timePassed / timeToComplete;
             characterImage.color = Color.Lerp(Color.white, Color.gray, progress);
 
@@ -157,6 +279,8 @@ public class ConversationCharacter : MonoBehaviour
             StopCoroutine(undimmingCoroutine);
         }
 
+        // TODO: Check for running dim coroutine and shut it down so there's no conflict.
+
         undimmingCoroutine = StartCoroutine(UndimCharacter(timeToComplete));
 
         // TODO: The else block may be unnecessary, or at least the yield return is. Right now it's basically waiting
@@ -175,6 +299,7 @@ public class ConversationCharacter : MonoBehaviour
 
         while(timePassed <= timeToComplete)
         {
+            // TODO: Account for divide by 0 case.
             float progress = timePassed / timeToComplete;
             characterImage.color = Color.Lerp(Color.gray, Color.white, progress);
 
