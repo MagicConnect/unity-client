ab47a6818360a9105d31aaf1c74f361437e0b8a6
civilityreigns <100637768+civilityreigns@users.noreply.github.com>
2022-05-13 10:34:29 -0500

    update cutscene build script [skip ci]

--- Assets/Editor/UnityBuilderAction/CutsceneStandaloneBuildScript.cs
+++ Assets/Editor/UnityBuilderAction/CutsceneStandaloneBuildScript.cs
@@ -141,8 +141,8 @@ namespace UnityBuilderAction
             }
 
             string[] scenes = new string[2];
-            EditorBuildSettingsScene loadingScene = EditorBuildSettings.scenes.Where(scene => scene.path == "testAssets/Scenes/Loading.unity").FirstOrDefault();
-            EditorBuildSettingsScene cutsceneScene = EditorBuildSettings.scenes.Where(scene => scene.path == "testAssets/Scenes/Yarn Demo.unity").FirstOrDefault();
+            EditorBuildSettingsScene loadingScene = EditorBuildSettings.scenes.Where(scene => scene.path == "Assets/Scenes/Loading.unity").FirstOrDefault();
+            EditorBuildSettingsScene cutsceneScene = EditorBuildSettings.scenes.Where(scene => scene.path == "Assets/Scenes/Yarn Demo.unity").FirstOrDefault();
 
             if(loadingScene != null && cutsceneScene != null)
             {
