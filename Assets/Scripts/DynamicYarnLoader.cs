669b4d0307cd0f28125b1ad5cf3e69b6f8ad8513
civilityreigns <100637768+civilityreigns@users.noreply.github.com>
2022-06-07 09:43:56 -0500

    rollback of Yarn Spinner version to fix bug

--- Assets/Scripts/DynamicYarnLoader.cs
+++ Assets/Scripts/DynamicYarnLoader.cs
@@ -11,6 +11,10 @@ public class DynamicYarnLoader : MonoBehaviour
 {
     private DialogueRunner dr;
 
+    public List<string> parseErrorMessages = new List<string>();
+
+    public bool isSuccessfullyParsed = false;
+
     public void Awake()
     {
         dr = GetComponent<DialogueRunner>();
@@ -138,6 +142,72 @@ public class DynamicYarnLoader : MonoBehaviour
             newProject.baseLocalization = newLocalization;
             newProject.localizations.Add(newLocalization);
             newLocalization.name = defaultLanguage;
+
+            dr.SetProject(newProject);
+        }
+    }
+
+    public void LoadScript_New(string sourceText, string assetPath = "default.yarn")
+    {
+        string fileName = System.IO.Path.GetFileNameWithoutExtension(assetPath);
+
+        Yarn.Program compiledProgram = null;
+        IDictionary<string, Yarn.Compiler.StringInfo> stringTable = null;
+
+        parseErrorMessages.Clear();
+
+        // Compile the source code into a compiled Yarn program (or
+        // generate a parse error)
+        var compilationJob = CompilationJob.CreateFromString(fileName, sourceText, null);
+        compilationJob.CompilationType = CompilationJob.Type.FullCompilation;
+
+        var result = Yarn.Compiler.Compiler.Compile(compilationJob);
+
+        IEnumerable<Diagnostic> errors = result.Diagnostics.Where(d => d.Severity == Diagnostic.DiagnosticSeverity.Error);
+
+        if (errors.Count() > 0)
+        {
+            isSuccessfullyParsed = false;
+
+            parseErrorMessages.AddRange(errors.Select(e =>
+            {
+                string message = $"{assetPath}: {e}";
+                //ctx.LogImportError($"Error importing {message}");
+                return message;
+            }));
+        }
+        else
+        {
+            isSuccessfullyParsed = true;
+            //LastImportHadImplicitStringIDs = result.ContainsImplicitStringTags;
+            //LastImportHadAnyStrings = result.StringTable.Count > 0;
+
+            stringTable = result.StringTable;
+            compiledProgram = result.Program;
+
+            YarnProject newProject = YarnProject.CreateInstance<YarnProject>();
+            byte[] compiledBytes = null;
+
+            using (var memoryStream = new MemoryStream())
+            using (var outputStream = new Google.Protobuf.CodedOutputStream(memoryStream))
+            {
+                // Serialize the compiled program to memory
+                compiledProgram.WriteTo(outputStream);
+                outputStream.Flush();
+
+                compiledBytes = memoryStream.ToArray();
+            }
+
+            newProject.compiledYarnProgram = compiledBytes;
+            newProject.name = "NAME";
+            // Dont know what these do. They were in the proper projects
+            /*yp.searchAssembliesForActions.Add("YarnSpinner.Unity");
+            yp.searchAssembliesForActions.Add("YarnSpinner.Unity.Editor");
+            yp.searchAssembliesForActions.Add("YarnSpinner.Editor.Tests");
+            yp.searchAssembliesForActions.Add("CommandsInAnAssemblyDefinition");
+            yp.searchAssembliesForActions.Add("YarnSpinnerTests");
+            yp.searchAssembliesForActions.Add("PsdPlugin");
+            yp.searchAssembliesForActions.Add("DocCodeExamples");*/
 
             dr.SetProject(newProject);
         }
