83fbd44bbb0cfb466d535db90d56547ccdaf0283
civilityreigns <100637768+civilityreigns@users.noreply.github.com>
2022-06-21 15:09:30 -0500

    load gm screen from home screen

--- Assets/Scripts/GMScreenUIController.cs
+++ Assets/Scripts/GMScreenUIController.cs
@@ -1,9 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using BestHTTP;
+
 public class GMScreenUIController : MonoBehaviour
 {
+    public event Action<string> OnResponseBodyReceived;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,32 @@ public class GMScreenUIController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OnGetResponseReceived(HTTPRequest request, HTTPResponse response)
+    {
+
+    }
+
+    public void OnPostResponseReceived(HTTPRequest request, HTTPResponse response)
+    {
+
+    }
+
+    public void OnPatchResponseReceived(HTTPRequest request, HTTPResponse response)
+    {
+
+    }
+
+    // Takes a BestHTTP request and dumps information about it to the console for testing.
+    public void LogRequest(HTTPRequest request)
+    {
+        Debug.Log(request.ToString());
+    }
+
+    // Takes a BestHTTP response and dumps information about it to the console for testing.
+    public void LogResponse(HTTPResponse response)
+    {
+        Debug.Log(response.ToString());
     }
 }
