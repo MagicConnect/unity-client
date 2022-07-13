6ce98aef936868da2dfad43af6ed04a5c983e685
civilityreigns <100637768+civilityreigns@users.noreply.github.com>
2022-07-13 13:56:01 -0500

    using characters request to create character cards

--- Assets/Scripts/CharacterCard.cs
+++ Assets/Scripts/CharacterCard.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 public class CharacterCard : MonoBehaviour
 {
@@ -18,16 +20,8 @@ public class CharacterCard : MonoBehaviour
     // The character's current level.
     public TMP_Text characterLevel;
 
-    private string _contentId;
-    public string contentId
-    {
-        get => _contentId;
-        set
-        {
-            _contentId = value;
-            RefreshUi();
-        }
-    }
+    // The parsed character information retrieved from the server.
+    public CharacterListUIController.Character character;
 
     // Start is called before the first frame update
     void Start()
@@ -41,9 +35,61 @@ public class CharacterCard : MonoBehaviour
         
     }
 
+    // When the character card is created, this method should be used to give it the information needed to display/animate.
+    public void SetCharacter(CharacterListUIController.Character character)
+    {
+        this.character = character;
+
+        RefreshUi();
+    }
+
     // Refreshes the character card ui elements to reflect any changes in data.
     public void RefreshUi()
     {
         // Use the current content id to find the character information in the cached game data.
+        JToken characters = GameDataCache.Instance.parsedGameData["characters"];
+
+        foreach(JToken character in characters.Children())
+        {
+            if(character["id"].Value<string>() == this.character.contentId)
+            {
+                // Use the character art name to get the character's headshot from the art cache, then create and set the sprite.
+                string artName = character["headArt"].Value<string>();
+
+                if(artName != "")
+                {
+                    Debug.LogFormat("Character Card: Using name '{0}' to create a new sprite.", artName);
+                    WebAssetCache.LoadedImageAsset asset = WebAssetCache.Instance.GetLoadedImageAssetByName(artName);
+
+                    if(asset != null)
+                    {
+                        Sprite newSprite = Sprite.Create(asset.texture, new Rect(0.0f, 0.0f, asset.texture.width, asset.texture.height), new Vector2(0.0f, 0.0f), 100.0f, 0, SpriteMeshType.FullRect);
+                        characterArt.sprite = newSprite;
+                    }
+                    else
+                    {
+                        // If we get a null reference back then the art doesn't exist in the cache (by the given name, anyway).
+                        Debug.LogErrorFormat(this, "Character Card: There was a problem retrieving the texture '{0}' from the asset cache.", artName);
+                    }
+                }
+                else
+                {
+                    // There was no art listed in the game data. Use a default sprite instead.
+                    Debug.LogErrorFormat(this, "Character Art: No art name was given for the attachment content. Using default sprite.");
+                }
+
+                // Set the character name text.
+                characterName.text = character["name"].Value<string>().ToUpper();
+
+                // Get the character's attack type and set the type icon sprite.
+
+                // Set the character level text.
+                characterLevel.text = this.character.currentLevel;
+
+                // Get the character's skill information and populate the skill list.
+
+                // Display the character's star rating.
+            }
+        }
     }
 }
---
+++ Assets/Scripts/CharacterListUIController.cs
@@ -0,0 +1,199 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using BestHTTP;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class CharacterListUIController : MonoBehaviour
+{
+    // The array of parsed character information downloaded from the server.
+    public class CharacterList
+    {
+        public Character[] characters;
+    }
+
+    // Information about a character provided by the server.
+    public class Character
+    {
+        public string id;
+
+        public string contentId;
+
+        public string currentXp;
+
+        public string currentLevel;
+
+        public string currentLb;
+
+        public string currentDupe;
+
+        public string activeDupe;
+
+        public string currentUserWeaponIdEquipped;
+
+        public Essences currentEssences;
+    }
+
+    // A character's parsed essence information provided by the server.
+    public class Essences
+    {
+        public int hp;
+
+        public int attack;
+
+        public int defense;
+
+        public int magic;
+
+        public int special;
+
+        public int critical;
+    }
+
+    // The firebase handler which we get our authentication token from.
+    FirebaseHandler firebase = FirebaseHandler.Instance;
+
+    // The list content gameobject which will all character cards will be attached to.
+    public GameObject characterListContainer;
+
+    // The prefab of the character cards that will be cloned for the character list.
+    public GameObject characterCardPrefab;
+
+    // The most up to date character list returned from the server.
+    public CharacterList mostRecentCharacterList;
+
+    // The character list background image.
+    public Image backgroundImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SendMeCharactersRequest();
+
+        // Set the default background image based on the design.
+        WebAssetCache.LoadedImageAsset imageAsset = WebAssetCache.Instance.GetLoadedImageAssetByName("TempleBase");
+
+        if(imageAsset != null)
+        {
+            Sprite newSprite = Sprite.Create(imageAsset.texture, new Rect(0.0f, 0.0f, imageAsset.texture.width, imageAsset.texture.height), new Vector2(0.0f, 0.0f), 100.0f, 0, SpriteMeshType.FullRect);
+            backgroundImage.sprite = newSprite;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Shows/hides character cards based on the attack type filter chosen.
+    public void FilterCharacterList()
+    {}
+
+    // Uses the most recent character list information to populate the character list with character cards.
+    public void RefreshCharacterList()
+    {
+        // Clear the character list ui of outdated cards.
+        // TODO: Instead of deleting everything, recycle old cards and/or update existing ones.
+        foreach(Transform card in characterListContainer.transform)
+        {
+            Destroy(card.gameObject);
+        }
+
+        // Create character cards for every character on the list.
+        foreach(Character character in mostRecentCharacterList.characters)
+        {
+            // Instantiate a copy of the character card prefab.
+            GameObject newCharacterCard = Instantiate(characterCardPrefab, characterListContainer.transform);
+
+            // Give the character card the character information it needs to work with. The card will handle its own animations.
+            newCharacterCard.GetComponent<CharacterCard>().SetCharacter(character);
+        }
+    }
+
+    // Parse a /me/characters response into objects that we can use.
+    public void ParseCharactersResponse(string data)
+    {
+        // Allow the JSON plugin to parse the information for us.
+        CharacterList characterList = JsonConvert.DeserializeObject<CharacterList>(data);
+
+        // Check the character list object to make sure everything was parsed correctly. If there was a problem parsing essential information,
+        // log an error so it can be fixed.
+        if(characterList != null)
+        {
+            if(characterList.characters == null)
+            {
+                Debug.LogErrorFormat(this, "Character List: There was a problem parsing the character list information retrieved from the server. JSON: {0}", data);
+                return;
+            }
+            else
+            {
+                // If everything was parsed okay, use the character list to update the ui.
+                mostRecentCharacterList = characterList;
+                RefreshCharacterList();
+            }
+        }
+    }
+
+    // Make a /me/characters request to the server.
+    public void SendMeCharactersRequest()
+    {
+        // Send a /me/characters request to the server.
+        HTTPRequest request = new HTTPRequest(new Uri("http://testserver.magic-connect.com/me/characters"), OnMeCharactersRequestFinished);
+
+        request.AddHeader("Authorization", string.Format("Bearer {0}", firebase.userToken));
+
+        request.Send();
+    }
+
+    // Gets the results of the me/characters request which will be used to create the character list.
+    public void OnMeCharactersRequestFinished(HTTPRequest request, HTTPResponse response)
+    {
+        switch(request.State)
+        {
+            // The request finished without any problem.
+            case HTTPRequestStates.Finished:
+                if(response.IsSuccess)
+                {
+                    // Dump all headers and their values into the console.
+                    Debug.LogFormat(this, "API Response Headers/Values:");
+                    foreach(string header in response.Headers.Keys)
+                    {
+                        Debug.LogFormat(this, "Header: {0} Value(s): {1}", header, response.Headers[header]);
+                    }
+
+                    Debug.LogFormat(this, "Response Data: {0}", response.DataAsText);
+                    Debug.LogFormat(this, "Status Code: {0} Message: {1}", response.StatusCode, response.Message);
+                    Debug.LogFormat(this, "Major Version: {0} Minor Version: {1}", response.VersionMajor, response.VersionMinor);
+                    Debug.LogFormat(this, "Was from cache: {0}", response.IsFromCache);
+
+                    ParseCharactersResponse(response.DataAsText);
+                }
+                else
+                {
+                    Debug.LogWarningFormat("Request finished successfully, but the server sent an error. Status Code: {0}--{1} Message: {2}", response.StatusCode, response.Message, response.DataAsText);
+                }
+                break;
+            // The request finished with an unexpected error. The request's Exception property may contain more info about the error.
+            case HTTPRequestStates.Error:
+                Debug.LogError("Request finished with an error: " + (request.Exception != null ? (request.Exception.Message + "\n" + request.Exception.StackTrace) : "No Exception"));
+                break;
+            // The request aborted, initiated by the user.
+            case HTTPRequestStates.Aborted:
+                Debug.LogWarning("Request aborted.");
+                break;
+            // Connecting to the server timed out.
+            case HTTPRequestStates.ConnectionTimedOut:
+                Debug.LogError("Connection timed out.");
+                break;
+            // The request didn't finish in the given time.
+            case HTTPRequestStates.TimedOut:
+                Debug.LogError("Processing the request timed out.");
+                break;
+        }// end switch block
+    }
+}
