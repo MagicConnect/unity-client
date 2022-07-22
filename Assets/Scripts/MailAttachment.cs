0ead3ff1f153ce60cf525001a816e5d79cbb221c
civilityreigns <100637768+civilityreigns@users.noreply.github.com>
2022-07-22 13:42:17 -0500

    removed commented code

--- Assets/Scripts/MailAttachment.cs
+++ Assets/Scripts/MailAttachment.cs
@@ -58,11 +58,10 @@ public class MailAttachment : MonoBehaviour
         string artName = "";
         string objectName = "";
 
-        JToken content = null;
+        //JToken content = null;
         switch(this.type)
         {
             case AttachmentType.Weapon: 
-                //content = GameDataCache.Instance.parsedGameData["weapons"];
                 GameDataCache.Instance.weaponsById.TryGetValue(contentId, out var weapon);
                 if(weapon != null)
                 {
@@ -75,7 +74,6 @@ public class MailAttachment : MonoBehaviour
                 }
                 break;
             case AttachmentType.Accessory: 
-                //content = GameDataCache.Instance.parsedGameData["accessories"];
                 GameDataCache.Instance.accessoriesById.TryGetValue(contentId, out var accessory);
                 if(accessory != null)
                 {
@@ -88,7 +86,6 @@ public class MailAttachment : MonoBehaviour
                 }
                 break;
             case AttachmentType.Item: 
-                //content = GameDataCache.Instance.parsedGameData["items"];
                 GameDataCache.Instance.itemsById.TryGetValue(contentId, out var item);
                 if(item != null)
                 {
@@ -101,7 +98,6 @@ public class MailAttachment : MonoBehaviour
                 }
                 break;
             case AttachmentType.Character: 
-                //content = GameDataCache.Instance.parsedGameData["characters"];
                 GameDataCache.Instance.charactersById.TryGetValue(contentId, out var character);
                 if(character != null)
                 {
@@ -139,43 +135,5 @@ public class MailAttachment : MonoBehaviour
             // There was no art listed in the game data. Use a default sprite instead.
             Debug.LogWarningFormat(this, "Mail Attachment: No art name was given for the attachment content. Using default sprite.");
         }
-
-/*
-        foreach(JToken contentObject in content.Children())
-        {
-            if(contentObject["id"].Value<string>() == this.contentId)
-            {
-                Debug.LogFormat("Content Object: {0}", contentObject.ToString());
-
-                // Get the name of the content and use it to set the label text.
-                attachmentName.text = string.Format("{0} x{1}", contentObject["name"].Value<string>(), this.quantity);
-
-                // Get the name of the sprite to be used and then retrieve it from the web asset cache.
-                string artName = contentObject["art"].Value<string>();
-
-                if(artName != "")
-                {
-                    Debug.LogFormat("Mail Attachment: Using name '{0}' to create a new sprite.", artName);
-                    WebAssetCache.LoadedImageAsset asset = WebAssetCache.Instance.GetLoadedImageAssetByName(artName);
-
-                    if(asset != null)
-                    {
-                        Sprite newSprite = Sprite.Create(asset.texture, new Rect(0.0f, 0.0f, asset.texture.width, asset.texture.height), new Vector2(0.0f, 0.0f), 100.0f, 0, SpriteMeshType.FullRect);
-                        attachmentImage.sprite = newSprite;
-                    }
-                    else
-                    {
-                        // If we get a null reference back then the art doesn't exist in the cache (by the given name, anyway).
-                        Debug.LogErrorFormat(this, "Mail Attachment: There was a problem retrieving the texture '{0}' from the asset cache.", artName);
-                    }
-                }
-                else
-                {
-                    // There was no art listed in the game data. Use a default sprite instead.
-                    Debug.LogWarningFormat(this, "Mail Attachment: No art name was given for the attachment content. Using default sprite.");
-                }
-            }
-        }
-        */
     }
 }
